a94dd7ece12144ebc91fe93d100d1356568e8d7a
Naume-k <kobnaome@gmail.com>
2019-09-06 14:42:55 +0200

    added Method that tests for saving multiple credentials

--- credentials_test.py
+++ credentials_test.py
@@ -12,10 +12,30 @@ class TestCredentials(unittest.TestCase):
         """
         self.new_credentials = Credentials("Istagram","instauser", "12345678")
 
-    def test_credentials_instance(self):
+    def test_init(self):
         """
         Method that tests whether the new_credentials have been instantiated correctly
         """
         self.assertEqual(self.new_credentials.account_name, "Istagram")
         self.assertEqual(self.new_credentials.account_user_name, "instauser")
         self.assertEqual(self.new_credentials.account_password, "12345678")
+
+    def test_save_credentials(self):
+        """
+        Method that tests whether the new credential created has been saved
+        """
+        self.new_credentials.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list), 1)
+
+    def test_save_multiple_credential(self):
+        '''
+        test_save_multiple_credentials to check if we can save multiple credential
+        objects to our credentials_list
+        '''
+        self.new_credentials.save_credential()
+        test_credentials = Credentials("Test","user","#0782386439?") # new contact
+        test_credentials.save_credential()
+        self.assertEqual(len(Credentials.credentials_list),2)
+
+        
+    
