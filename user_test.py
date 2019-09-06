c8673bc5a74ce44b179aee5f43be9b8ceb367aa4
Naume-k <kobnaome@gmail.com>
2019-09-06 10:21:32 +0200

    defined the test cases

--- user_test.py
+++ user_test.py
@@ -1,10 +1,10 @@
 import unittest
 from user import User
 
-# class TestUser(unittest.TestCase):
-#     """
-#     Test class that defines test cases for the user class behaviours.
-#     """
+class TestUser(unittest.TestCase):
+    """
+    Test class that defines test cases for the user class behaviours.
+    """
 #     def setUp(self):
 #         '''
 #         Set up method to run before each test cases.
