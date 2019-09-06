268f401aefa31ec2a006bc053b27a4b891bb96ae
Naume-k <kobnaome@gmail.com>
2019-09-06 10:16:35 +0200

    created the necessary files

---
+++ user.py
@@ -0,0 +1,19 @@
+# class User:
+#     """
+#     Class that generates new instances of a user
+#     """
+
+#     def __init__(self, user_name, password):
+#         self.user_name = user_name
+#         self.password = password
+
+#     user_list = []    #empty user list
+
+#     def save_user(self):
+#         """
+#         save_user method saves user objects into user_list
+#         """
+#         self.user_list.append(self)
+
+# if __name__ == '__main__':
+#     main()
---
+++ user_test.py
@@ -0,0 +1,30 @@
+# import unittest
+# from user import User
+
+# class TestUser(unittest.TestCase):
+#     """
+#     Test class that defines test cases for the user class behaviours.
+#     """
+#     def setUp(self):
+#         '''
+#         Set up method to run before each test cases.
+#         '''
+#         self.new_user = User("App_User", "12345678")
+
+#     def test_init(self):
+#         """
+#         Testing to ensure that the object is initialized properly
+#         """
+#         self.assertEqual(self.new_user.user_name, "App_User")
+#         self.assertEqual(self.new_user.password, "12345678")
+
+#     def test_save_user(self):
+#         """
+#         Method that tests whether user credentials have been successfully saved
+#         """
+#         self.new_user.save_user()
+#         self.assertEqual(len(User.user_list), 1)
+
+# if __name__ == '__main__':
+#     unittest.main()
+    
