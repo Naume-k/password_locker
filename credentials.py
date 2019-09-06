1aea5d9ff364d8a1d84e903bd7bb3ead2ebe45fc
Naume-k <kobnaome@gmail.com>
2019-09-06 11:13:47 +0200

    added the init method and created an empty list

--- credentials.py
+++ credentials.py
@@ -2,4 +2,11 @@ class Credentials:
     """
     Class that generates new instances of the credentials
     """
+    def __init__(self, account_name,account_user_name, account_password):
+        self.account_name = account_name
+        self.account_user_name = account_user_name
+        self.account_password = account_password
+
+    credentials_list = []
+
     
