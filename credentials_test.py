288237cd7861a6b545fe3451072ea7850a6d3de5
Naume-k <kobnaome@gmail.com>
2019-09-06 12:21:52 +0200

    Test class that defines test cases for the Credentials class behavior

--- credentials_test.py
+++ credentials_test.py
@@ -1,2 +1,8 @@
 import unittest
 from credentials import Credentials
+
+class TestCredentials(unittest.TestCase):
+    """
+    Test class that defines test cases for the Credentials class behavior
+    """
+    
