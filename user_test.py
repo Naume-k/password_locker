49c4ded06ceff29ddd7e54b033da6d3f04901032
Naume-k <kobnaome@gmail.com>
2019-09-06 10:23:23 +0200

    added a test to ensure the object is initialised properly

--- user_test.py
+++ user_test.py
@@ -11,12 +11,12 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user = User("App_User", "12345678")
 
-#     def test_init(self):
-#         """
-#         Testing to ensure that the object is initialized properly
-#         """
-#         self.assertEqual(self.new_user.user_name, "App_User")
-#         self.assertEqual(self.new_user.password, "12345678")
+    def test_init(self):
+        """
+        Testing to ensure that the object is initialized properly
+        """
+        self.assertEqual(self.new_user.user_name, "App_User")
+        self.assertEqual(self.new_user.password, "12345678")
 
 #     def test_save_user(self):
 #         """
